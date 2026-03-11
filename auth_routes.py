02146cea73ec2a03248702aef9b897769aad33be
RC-Figueiredo <renato.figueiredo1109@gmail.com>
2026-03-11 10:22:30 -0300

    finalizacao das rotas,inicio do banco

--- auth_routes.py
+++ auth_routes.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
 
-auth_router = APIRouter(prefix="/auth")
+auth_router = APIRouter(prefix="/auth", tags=["lista"])
+
+@auth_router.get("/")
+async def autenticar():
+    """
+    O Usuario so podera acessar esta rota sse for autenticado
+    """
+    return{"Mensagem":" tu acessou a rota de autenticação"}
--- order_routes.py
+++ order_routes.py
@@ -1,3 +1,11 @@
 from fastapi import APIRouter
 
-order_router= APIRouter(prefix="/order", tags=["auth"])
+order_router= APIRouter(prefix="/pedidos", tags=["pedidos"])
+
+@order_router.get("/")
+async def pedidos():
+    """
+    Esta rota é dedicada aos pedidos 
+    """
+    return {"mensagem":" Você acessou a rota pedidos"}
+      
