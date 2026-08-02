d708883f0f1469975b7ffbd5dd8c0f6c8bbe38df
RC-Figueiredo <renato.figueiredo1109@gmail.com>
2026-08-02 18:03:15 -0300

    Criacao de rotas

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
-#para rodar o codigo usar o comando: uvicorn main:app --reload
+#* para rodar o porjeto utilize o comando python -m uvicorn main:app --reload *#
 from fastapi import FastAPI
-from fastapi.security import OAuth2PasswordBearer
-from passlib.context import CryptContext
-from dotenv import load_dotenv 
-import os
 
-load_dotenv()
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM=os.getenv("ALGORITHM")
-#*necessario transforma-lo em numero inteiro,para indicar o tempo de expiração do token*#
-ACESS_TOKEN_EXPIRE_MINUTES= int(os.getenv("ACESS_TOKEN_EXPIRE_MINUTES"))
+app= FastAPI()  
 
-app = FastAPI()
-
-bcrypt_context = CryptContext(schemes=["bcrypt"],deprecated = "auto")
-oauth2_schema = OAuth2PasswordBearer(tokenUrl="/auth/login-form")
-
+# importar os roteadores de requisições
 from auth_routes import auth_router
 from order_routes import order_router
 
+# incluir os roteadores a aba main
 app.include_router(auth_router)
-app.include_router(order_router) 
+app.include_router(order_router)
